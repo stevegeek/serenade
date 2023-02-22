2eafb4eae559b0fc7eb342f187f2a7156317962d
Stephen Ierodiaconou <stevegeek@gmail.com>
2023-02-22 16:27:12 +0100

    Update to boost 1.81.0 See https://github.com/Homebrew/homebrew-core/issues/67427

--- scripts/setup/build-dependencies.sh
+++ scripts/setup/build-dependencies.sh
@@ -76,7 +76,7 @@ cd ..
 
 git clone --recursive https://github.com/boostorg/boost.git boost-src
 cd boost-src
-git checkout boost-1.78.0
+git checkout boost-1.81.0
 ./bootstrap.sh --prefix=$PWD/../boost
 ./b2 install
 cd ..
