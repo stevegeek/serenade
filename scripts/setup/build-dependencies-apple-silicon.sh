3cc467e6cfcea62300a7243703d37e9215d109b2
Stephen Ierodiaconou <stevegeek@gmail.com>
2023-02-22 16:41:45 +0100

    Execute all compilation related commands with rosetta to ensure build target it x86

--- scripts/setup/build-dependencies-apple-silicon.sh
+++ scripts/setup/build-dependencies-apple-silicon.sh
@@ -89,22 +89,22 @@ rm icu4c-72_1-src.tgz
 git clone --recursive https://github.com/boostorg/boost.git boost-src
 cd boost-src
 git checkout boost-1.81.0
-./bootstrap.sh --prefix=$PWD/../boost
-./b2 install
+arch -x86_64 ./bootstrap.sh --prefix=$PWD/../boost
+arch -x86_64 ./b2 install
 cd ..
 rm -rf boost-src
 
 git clone --recursive -b v3.14.0 https://github.com/protocolbuffers/protobuf.git protobuf-src
 mkdir -p protobuf
 cd protobuf-src
-./autogen.sh
-./configure --prefix=$PWD/../protobuf --disable-shared --with-pic
+arch -x86_64 ./autogen.sh
+arch -x86_64 ./configure --prefix=$PWD/../protobuf --disable-shared --with-pic
 if [[ `uname` == "Darwin" ]] ; then
-  make CFLAGS="-mmacosx-version-min=$osx_version" CXXFLAGS="-g -std=c++11 -DNDEBUG -mmacosx-version-min=$osx_version" -j2
+  arch -x86_64 make CFLAGS="-mmacosx-version-min=$osx_version" CXXFLAGS="-g -std=c++11 -DNDEBUG -mmacosx-version-min=$osx_version" -j2
 else
-  make -j2
+  arch -x86_64 make -j2
 fi
-make install
+arch -x86_64 make install
 cd ..
 rm -rf protobuf-src
 
@@ -160,22 +160,22 @@ git checkout 3ec108da76e3d9dba901fb69f046d0e46170b8e7
 git apply $SERENADE_SOURCE_ROOT/patches/kaldi/stateless.patch
 cd tools
 if [[ `uname` == 'Darwin' ]] ; then
-  perl -i -pe"s/-g -O2/-g -O2 -mmacosx-version-min=$osx_version/g" Makefile
+  arch -x86_64 perl -i -pe"s/-g -O2/-g -O2 -mmacosx-version-min=$osx_version/g" Makefile
 fi
-make -j2
+arch -x86_64 make -j2
 cd ../src
 if [[ `uname` == 'Darwin' ]] ; then
-  ./configure --shared --use-cuda=no
-  perl -i -pe"s/-O1/-O3 -DNDEBUG -mmacosx-version-min=$osx_version/g" kaldi.mk
+  arch -x86_64 ./configure --shared --use-cuda=no
+  arch -x86_64 perl -i -pe"s/-O1/-O3 -DNDEBUG -mmacosx-version-min=$osx_version/g" kaldi.mk
 else
   ./configure --shared --mathlib=MKL --use-cuda=no
   perl -i -pe's/-O1/-O3 -DNDEBUG/g' kaldi.mk
 fi
-perl -i -pe's/-g //g' kaldi.mk
-make -j clean depend
-make -j2
+arch -x86_64 perl -i -pe's/-g //g' kaldi.mk
+arch -x86_64 make -j clean depend
+arch -x86_64 make -j2
 cd ../tools
-./extras/install_phonetisaurus.sh
+arch -x86_64 ./extras/install_phonetisaurus.sh
 cd ../..
 
 if [[ "$minimal" == "true" ]] ; then
